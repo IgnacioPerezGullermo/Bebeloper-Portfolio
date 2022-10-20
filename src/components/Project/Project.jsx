f25d1c98b15b4b5fc731a0f852ea2943c62a7041
El Bebelo <nacho71197@gmail.com>
2022-10-20 11:26:45 -0300

    Arreglo del Portfolio

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from '../../index.scss';
 import TLF from '../../assets/TLF.png';
 import Dogs from '../../assets/Dogs.png';
+import LeafMe from '../../assets/LeafMe.png';
+//import TS from '../../assets/typescript.svg';
 import Tooltip from '@mui/material/Tooltip';
 import node from '../../assets/nodelogo.png';
 
@@ -37,6 +39,7 @@ export const Project = ({ sourceLang }) => {
             />
           </Tooltip>
         </div>
+
         <div className="logo-container postgres-logo-container">
           <Tooltip title="PostgreSQL" placement="top">
             <img
@@ -76,6 +79,45 @@ export const Project = ({ sourceLang }) => {
           </Tooltip>
         </div>
       </div>
+      <div className="techs-wrap-leafme">
+        <div className="logo-container node-logo-container">
+          <Tooltip title="Nest JS" placement="top">
+            <img
+              className="node-logo"
+              src="https://www.vectorlogo.zone/logos/nestjs/nestjs-icon.svg"
+              alt="node-logo"
+            />
+          </Tooltip>
+        </div>
+        <div className="logo-container react-logo-container">
+          <Tooltip title="React JS" placement="top">
+            <img
+              className="react-logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+              alt="node-logo"
+            />
+          </Tooltip>
+        </div>
+
+        <div className="logo-container mongodb-logo-container">
+          <Tooltip title="MongoDB" placement="top">
+            <img
+              className="mongodb-logo"
+              src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg"
+              alt="node-logo"
+            />
+          </Tooltip>
+        </div>
+        <div className="logo-container typescript-logo-container">
+          <Tooltip title="TypeScript" placement="top">
+            <img
+              className="typescript-logo"
+              src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
+              alt="node-logo"
+            />
+          </Tooltip>
+        </div>
+      </div>
       <div className="project tlf">
         <div className="arrow-techs-tlf"></div>
 
@@ -88,6 +130,18 @@ export const Project = ({ sourceLang }) => {
             <p className="info-description">
               {sourceLang.Project.rockband.desc}
             </p>
+          </a>
+        </div>
+      </div>
+      <div className="project leafme">
+        <div className="arrow-techs-dogs"></div>
+        <a href="https://final-project-client-uipg.vercel.app">
+          <img src={LeafMe} alt="tlf foto" />
+        </a>
+        <div className="info-container">
+          <a href="https://pi-dogs-henry-flame.vercel.app">
+            <p className="info">Leaf Me</p>
+            <p className="info-description">{sourceLang.Project.leafme.desc}</p>
           </a>
         </div>
       </div>
